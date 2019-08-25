1b79e75560bc7c4c7c30eecd2fe2c69c6035b351
niteshrawat <nitesh.rawat@manprax.com>
2019-08-25 22:30:38 +0530

    1. models fields

--- DjangoBasics/model_admin/admin.py
+++ DjangoBasics/model_admin/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Source, Content
 # Register your models here.
+
+
+@admin.register(Source)
+class SourceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    pass
--- DjangoBasics/model_admin/models.py
+++ DjangoBasics/model_admin/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+SOURCE_TYPE = (
+    ('edx', 'Edx'),
+    ('connect', 'Connect'),
+)
+
+
+class MXCatalog(models.Model):
+    """
+    Base Class for this app's models.
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Source(MXCatalog):
+    """
+    what does model represent?
+    """
+    name = models.CharField(verbose_name=_("Source name"), max_length=255)
+    type = models.CharField(verbose_name=_(
+        "Source Type"), choices=SOURCE_TYPE, max_length=255)
+    title = models.CharField(verbose_name=_("Source Title"), max_length=255)
+    order = models.PositiveIntegerField(
+        verbose_name=_("Source order"), default=0)
+
+    created_by = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name=_("source_created_by"))
+    modified_by = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name=_("source_modified_by"))
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _("Content Source")
+        verbose_name_plural = _("Content Sources")
+        unique_together = (("name", "title"), )
+
+
+# {media_root}/{instance.id}/example.png
+def upload_location(instance, filename):
+    return "{}/{}".format(instance.pk, filename)
+
+
+class Content(MXCatalog):
+    """
+    what does model represent?
+    """
+    source = models.ForeignKey(to=Source, on_delete=models.CASCADE)
+
+    name = models.CharField(verbose_name=_("Source name"), max_length=255)
+    slug = models.SlugField(verbose_name=_("Content Slug"), unique=True)
+    icon = models.ImageField(verbose_name=_(
+        "Content icon"), max_length=255, blank=True, null=True)
+    doc = models.FileField(verbose_name=_("Doc File"),
+                           upload_to=upload_location, null=True, blank=True)
+
+    created_by = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name=_("content_created_by"))
+    modified_by = models.ForeignKey(
+        to=User, on_delete=models.CASCADE, related_name=_("content_modified_by"))
